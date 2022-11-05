aa0b834043be9fa710fa17ea5c65f09a049606a2
mayumonj <40317050+mayumonj@users.noreply.github.com>
2022-11-05 19:50:20 +0900

    TODOリストの追加・編集・削除（データ保存は未対応）

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+let id = 0
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTodo: '',
+      todos: [
+        { id: id++, text: 'Learn HTML' , editing: false},
+        { id: id++, text: 'Learn JavaScript' , editing: false},
+        { id: id++, text: 'Learn Vue' , editing: false}
+      ],
+      flag: false
+    }
+  },
+  computed: {
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() === '') {
+        this.newTodo = ''
+        return
+      }
+      this.todos.push({ id: id++, text: this.newTodo, editing: false })
+      this.newTodo = ''
+    },
+    editTodo(todo) {
+      this.todos.forEach((item) => {
+        if (item.id === todo.id) {
+          item.editing = true
+        }
+      })
+    },
+    doneEdit(todo) {
+      this.todos.forEach((item) => {
+        if (item.id === todo.id) {
+          item.editing = false
+        }
+      })
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo)
+    }
   }
 }
 </script>
 
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>
+  </form>
+  <table>
+    <tr>
+      <th>Todo</th>
+      <th></th>
+      <th></th>
+    </tr>
+    <tr v-for="todo in todos" :key="todo.id">
+      <td v-if="todo.editing"><input type="text" v-model="todo.text"></td>
+      <td v-else>{{ todo.text }}</td>
+      <td v-if="todo.editing"><button @click="doneEdit(todo)">done</button></td>
+      <td v-else><button @click="editTodo(todo)">edit</button></td>
+      <td><button @click="removeTodo(todo)">remove</button></td>
+    </tr>
+  </table>
+</template>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 20px;
+}
+table {
+  margin-top: 20px;
+}
+td {
+  padding: 5px;
 }
 </style>
